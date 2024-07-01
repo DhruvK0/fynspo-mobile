134b0a6fd7b7ba9cbefa855b6fc9e859c3b1be8d
Dhruv Kanetkar <kanetkard@gmail.com>
2024-06-30 20:18:23 -0700

    removed unused import

--- fynspo/components/Profile.js
+++ fynspo/components/Profile.js
@@ -1,5 +1,6 @@
 import {View, Text} from 'react-native';
 import { SignOut } from './Auth/SignOut';
+
 export default function ProfileScreen() {
   return (
     <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
