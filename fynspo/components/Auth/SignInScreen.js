34c8f8969c522c554383797f1526e0b6fb3d0d28
Dhruv Kanetkar <kanetkard@gmail.com>
2024-06-30 18:58:11 -0700

    sign in ui changes

--- fynspo/components/Auth/SignInScreen.js
+++ fynspo/components/Auth/SignInScreen.js
@@ -1,10 +1,9 @@
 import React from "react";
-import { Text, TextInput, TouchableOpacity, View } from "react-native";
+import { Text, TextInput, TouchableOpacity, View, StyleSheet } from "react-native";
 import { useSignIn } from "@clerk/clerk-expo";
 
 export default function SignInScreen() {
   const { signIn, setActive, isLoaded } = useSignIn();
-
   const [emailAddress, setEmailAddress] = React.useState("");
   const [password, setPassword] = React.useState("");
 
@@ -25,31 +24,68 @@ export default function SignInScreen() {
       console.log(err);
     }
   };
+
   return (
-    <View>
-      <View>
+    <View >
+      <View style={styles.inputContainer}>
         <TextInput
           autoCapitalize="none"
           value={emailAddress}
           placeholder="Email..."
+          placeholderTextColor="black"
           onChangeText={(emailAddress) => setEmailAddress(emailAddress)}
-          style={{backgroundColor: 'white'}}
+          style={styles.input}
         />
       </View>
 
-      <View>
+      <View style={styles.inputContainer}>
         <TextInput
           value={password}
           placeholder="Password..."
+          placeholderTextColor="black"
           secureTextEntry={true}
           onChangeText={(password) => setPassword(password)}
-          style={{backgroundColor: 'white'}}
+          style={styles.input}
         />
       </View>
 
-      <TouchableOpacity onPress={onSignInPress} style={{backgroundColor: 'white'}}>
-        <Text>Sign in</Text>
+      <TouchableOpacity onPress={onSignInPress} style={styles.button}>
+        <Text style={styles.buttonText}>Sign in</Text>
       </TouchableOpacity>
     </View>
   );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    backgroundColor: '#8A2BE2', // Purple background
+    borderRadius: 10,
+    padding: 15,
+  },
+  inputContainer: {
+    marginBottom: 10,
+    color: 'black',
+  },
+  input: {
+    backgroundColor: '#e6e6e6',
+    borderRadius: 25,
+    padding: 10,
+    fontSize: 16,
+    color: 'black',
+  },
+  button: {
+    backgroundColor: '#8A2BE2', // Purple background
+    borderRadius: 25,
+    padding: 12,
+    alignItems: 'center',
+    marginTop: 10,
+    color: 'black',
+    marginVertical: 10,
+  },
+  buttonText: {
+    color: 'white',
+    fontSize: 18,
+    fontWeight: 'bold',
+    
+  },
+});
