d436bb3ba03184640154bb21775902d05b8ad244
Dhruv Kanetkar <kanetkard@gmail.com>
2024-07-10 19:59:07 -0700

    eas build configs

--- fynspo/app.config.js
+++ fynspo/app.config.js
@@ -3,5 +3,8 @@ module.exports = {
   version: '1.0.0',
   extra: {
     clerkPublishableKey: process.env.EXPO_PUBLIC_CLERK_PUBLISHABLE_KEY,
+    eas: {
+      projectId: "52c280fa-9736-45f4-bee3-46b72f98288e",
+    }
   },
 };
