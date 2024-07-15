411818fefebfe7f6650fbb35db1c1d8ce53829d2
Dhruv Kanetkar <kanetkard@gmail.com>
2024-07-15 16:20:12 -0700

    token error logging

--- fynspo/App.js
+++ fynspo/App.js
@@ -10,8 +10,16 @@ import * as ScreenOrientation from 'expo-screen-orientation';
 const tokenCache = {
   async getToken(key) {
     try {
-      return SecureStore.getItemAsync(key);
-    } catch (err) {
+      const item = await SecureStore.getItemAsync(key);
+      if (item) {
+        console.log(`${key} was used 🔐 \n`);
+      } else {
+        console.log("No values stored under key: " + key);
+      }
+      return item;
+    } catch (error) {
+      console.error("SecureStore get item error: ", error);
+      await SecureStore.deleteItemAsync(key);
       return null;
     }
   },
