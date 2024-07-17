ed9ba7b4a11d8500041cf1ab104372b2bf9aba33
Dhruv Kanetkar <kanetkard@gmail.com>
2024-07-17 11:46:53 -0700

    build 6 and app icon setup

--- fynspo/app.config.js
+++ fynspo/app.config.js
@@ -7,12 +7,20 @@ module.exports = {
     clerkPublishableKey: process.env.EXPO_PUBLIC_CLERK_PUBLISHABLE_KEY,
     eas: {
       projectId: "601bbef9-cc0a-41fc-b7a0-e263683950d4",
-    }
+    },
+    icon: "./assets/images/icon.png"
   },
   ios: {  
       bundleIdentifier: 'com.fynspo.app',
-      buildNumber: '5'
+      buildNumber: '6'
     }
   },
+  android: {
+    package: 'com.fynspo.app',
+    versionCode: 6,
+    adaptiveIcon: {
+      foregroundImage: './assets/images/adaptive-icon.png',
+    }
+  }
    
 };
